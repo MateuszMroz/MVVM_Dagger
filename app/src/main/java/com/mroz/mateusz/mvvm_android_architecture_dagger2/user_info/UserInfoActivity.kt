5e8e08d855babcdb331aded35c3ada21ceb8501d
Mateusz Mroz <mateusz.mroz.programista@gmail.com>
2019-01-05 22:03:07 +0100

    add xml file for user info

---
+++ app/src/main/java/com/mroz/mateusz/mvvm_android_architecture_dagger2/user_info/UserInfoActivity.kt
@@ -0,0 +1,17 @@
+package com.mroz.mateusz.mvvm_android_architecture_dagger2.user_info
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mroz.mateusz.mvvm_android_architecture_dagger2.R
+import com.mroz.mateusz.mvvm_android_architecture_dagger2.databinding.ActivityUserInfoBinding
+
+class UserInfoActivity : AppCompatActivity() {
+
+     lateinit var binding:ActivityUserInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_info)
+    }
+}
